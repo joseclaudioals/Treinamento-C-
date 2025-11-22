2ad44ba9501b0fc842015fca65d814349a9ae449
joseclaudioals <joseclaudioals1092gmail.com>
2025-11-22 19:41:22 -0300

    questao 4

---
+++ Estrutura_de_Dados/Alocacao_Dinamica_Memoria/Questao4/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Produto{
+    char nome[30];
+    float preco;
+    int quantidade;
+}Produto;
+
+void limparBuffer(){
+    int c;
+    while((c=getchar())!='\n' && c!=EOF){}
+}
+
+int main(void){
+    Produto* estoque = malloc(5*sizeof(Produto));
+
+    printf(">> ESTOQUE <<\n");
+    for(int i=0; i<5; i++){
+        printf("\n- Produto %i\n", i+1);
+        printf("Insira o nome do produto:\n");
+        scanf(" %s", estoque[i].nome);
+        limparBuffer();
+
+        printf("Insira o preco do produto:\n");
+        scanf(" %f", &estoque[i].preco);
+
+        printf("Insira a quantidade do produto:\n");
+        scanf(" %i", &estoque[i].quantidade);
+    }
+    for(int i=0; i<5; i++){
+        printf("\n- Produto %i\n", i+1);
+        printf("Nome: %s\n",estoque[i].nome);
+        printf("Preco: R$ %.2f\n", estoque[i].preco);
+        printf("Quantidade: %i\n", estoque[i].quantidade);
+    }
+
+    free(estoque);
+    return 0;
+}
